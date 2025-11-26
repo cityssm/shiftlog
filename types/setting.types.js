9dd4a3ab3aa6d905ae749f84077ef4a77a9ecfeb
Dan Gowans <dangowans@users.noreply.github.com>
2025-11-26 09:10:49 -0500

    fix broken type compiles

--- types/setting.types.js
+++ types/setting.types.js
@@ -1,9 +1,6 @@
-"use strict";
 // eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
 /* eslint-disable perfectionist/sort-objects */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.settingProperties = void 0;
-exports.settingProperties = [
+export const settingProperties = [
     {
         settingKey: 'application.csrfSecret',
         settingName: 'Application - CSRF Secret',
