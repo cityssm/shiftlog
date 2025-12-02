593225a841757c315b6fca07afba372a8441b6d3
Dan Gowans <dangowans@users.noreply.github.com>
2025-12-02 09:34:41 -0500

    sync

--- public/javascripts/workOrders.viewEdit.js
+++ public/javascripts/workOrders.viewEdit.js
@@ -1,6 +1,42 @@
 (() => {
+    const shiftLog = exports.shiftLog;
     const workOrderTabsContainerElement = document.querySelector('#container--workOrderTabs');
     if (workOrderTabsContainerElement !== null) {
-        exports.shiftLog.initializeRecordTabs(workOrderTabsContainerElement);
+        shiftLog.initializeRecordTabs(workOrderTabsContainerElement);
+    }
+    /*
+     * Reopen work order
+     */
+    const reopenWorkOrderButton = document.querySelector('#button--reopenWorkOrder');
+    if (reopenWorkOrderButton !== null) {
+        reopenWorkOrderButton.addEventListener('click', () => {
+            bulmaJS.confirm({
+                contextualColorName: 'warning',
+                message: 'Are you sure you want to reopen this work order? This will clear the close date.',
+                okButton: {
+                    callbackFunction: () => {
+                        const workOrderIdElement = document.querySelector('#workOrder--workOrderId');
+                        cityssm.postJSON(`${shiftLog.urlPrefix}/${shiftLog.workOrdersRouter}/doReopenWorkOrder`, {
+                            workOrderId: workOrderIdElement.value
+                        }, (rawResponseJSON) => {
+                            const responseJSON = rawResponseJSON;
+                            if (responseJSON.success &&
+                                responseJSON.redirectUrl !== undefined) {
+                                globalThis.location.href = responseJSON.redirectUrl;
+                            }
+                            else {
+                                bulmaJS.alert({
+                                    contextualColorName: 'danger',
+                                    message: responseJSON.errorMessage ?? 'An unknown error occurred.',
+                                    title: 'Reopen Error'
+                                });
+                            }
+                        });
+                    },
+                    text: 'Reopen Work Order'
+                },
+                title: 'Reopen Work Order'
+            });
+        });
     }
 })();
