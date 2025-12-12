67efebb10065e33687e96c06d423ce6f4f4f38b9
Dan Gowans <dangowans@users.noreply.github.com>
2025-12-12 10:40:51 -0500

    link to the digest from the reports

--- handlers/dashboard-get/reports.js
+++ handlers/dashboard-get/reports.js
@@ -1,7 +1,10 @@
-export default function handler(request, response) {
+import getAssignedToDataListItems from '../../database/workOrders/getAssignedToDataListItems.js';
+export default async function handler(request, response) {
     const activeTab = request.query.tab ?? '';
+    const assignedToDataListItems = await getAssignedToDataListItems();
     response.render('dashboard/reports', {
         headTitle: 'Reports and Exports',
-        activeTab
+        activeTab,
+        assignedToDataListItems
     });
 }
