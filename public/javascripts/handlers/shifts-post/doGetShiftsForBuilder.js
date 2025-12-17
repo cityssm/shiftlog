d33ba8570a2b2d7528f24aa9a1b0fe19b792baf8
Dan Gowans <dangowans@users.noreply.github.com>
2025-12-17 13:07:23 -0500

    remove duplicated code

--- public/javascripts/handlers/shifts-post/doGetShiftsForBuilder.js
+++
@@ -1,8 +0,0 @@
-import getShiftsForBuilder from '../../database/shifts/getShiftsForBuilder.js';
-export default async function handler(request, response) {
-    const shifts = await getShiftsForBuilder(request.body.shiftDateString, request.session.user);
-    response.json({
-        success: true,
-        shifts
-    });
-}
--- public/javascripts/types/user.types.js
+++
@@ -1,7 +0,0 @@
-export const userSettingKeys = [
-    'apiKey',
-    'timesheets.canMarkEmployeesAsEntered',
-    'timesheets.canMarkEquipmentAsEntered',
-    // eslint-disable-next-line no-secrets/no-secrets
-    'workOrders.defaultAssignedToDataListItemId'
-];
