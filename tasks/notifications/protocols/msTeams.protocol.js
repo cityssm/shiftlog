23527f7eb75ccbbbb9b17761d799279193e45349
Dan Gowans <dangowans@users.noreply.github.com>
2026-01-23 13:37:24 -0500

    basic ms teams notification implementation

--- tasks/notifications/protocols/msTeams.protocol.js
+++ tasks/notifications/protocols/msTeams.protocol.js
@@ -1,6 +1,55 @@
+import sendMessageToTeamsWebhook from '@cityssm/ms-teams-workflow-webhook';
+import { recordToFactSet } from '@cityssm/ms-teams-workflow-webhook/helpers';
+import { getWorkOrderUrl } from '../../../helpers/application.helpers.js';
+import { getConfigProperty } from '../../../helpers/config.helpers.js';
+import { getWorkOrderToSend } from '../helpers/workOrder.helpers.js';
 export const sendWorkOrderCreateMsTeamsNotification = async (notificationConfiguration, workOrderId) => {
-    throw new Error('Not implemented yet');
+    const workOrderToSend = await getWorkOrderToSend(workOrderId, notificationConfiguration);
+    if (!workOrderToSend?.success) {
+        return workOrderToSend;
+    }
+    const workOrder = workOrderToSend.workOrder;
+    const msTeamsSpecificConfig = JSON.parse(notificationConfiguration.notificationTypeFormJson);
+    await sendMessageToTeamsWebhook(msTeamsSpecificConfig.webhookUrl, [
+        {
+            type: 'TextBlock',
+            text: `New ${workOrder.workOrderType}: ${workOrder.workOrderNumber}`,
+            weight: 'bolder'
+        },
+        recordToFactSet({
+            Priority: workOrder.workOrderPriorityDataListItem,
+            Status: workOrder.workOrderStatusDataListItem,
+            Details: workOrder.workOrderDetails
+        })
+    ], {
+        type: 'Action.OpenUrl',
+        title: `View ${getConfigProperty('workOrders.sectionNameSingular')}`,
+        url: getWorkOrderUrl(workOrder.workOrderId)
+    });
+    return { success: true };
 };
 export const sendWorkOrderUpdateMsTeamsNotification = async (notificationConfiguration, workOrderId) => {
-    throw new Error('Not implemented yet');
+    const workOrderToSend = await getWorkOrderToSend(workOrderId, notificationConfiguration);
+    if (!workOrderToSend?.success) {
+        return workOrderToSend;
+    }
+    const workOrder = workOrderToSend.workOrder;
+    const msTeamsSpecificConfig = JSON.parse(notificationConfiguration.notificationTypeFormJson);
+    await sendMessageToTeamsWebhook(msTeamsSpecificConfig.webhookUrl, [
+        {
+            type: 'TextBlock',
+            text: `Updated ${workOrder.workOrderType}: ${workOrder.workOrderNumber}`,
+            weight: 'bolder'
+        },
+        recordToFactSet({
+            Priority: workOrder.workOrderPriorityDataListItem,
+            Status: workOrder.workOrderStatusDataListItem,
+            Details: workOrder.workOrderDetails
+        })
+    ], {
+        type: 'Action.OpenUrl',
+        title: `View ${getConfigProperty('workOrders.sectionNameSingular')}`,
+        url: getWorkOrderUrl(workOrder.workOrderId)
+    });
+    return { success: true };
 };
--- tasks/notifications/protocols/msTeams.protocol.ts
+++ tasks/notifications/protocols/msTeams.protocol.ts
@@ -1,5 +1,12 @@
+import sendMessageToTeamsWebhook from '@cityssm/ms-teams-workflow-webhook'
+import { recordToFactSet } from '@cityssm/ms-teams-workflow-webhook/helpers'
+
+import { getWorkOrderUrl } from '../../../helpers/application.helpers.js'
+import { getConfigProperty } from '../../../helpers/config.helpers.js'
 import type { NotificationConfiguration } from '../../../types/record.types.js'
+import { getWorkOrderToSend } from '../helpers/workOrder.helpers.js'
 import type {
+  MsTeamsNotificationConfig,
   NotificationFunction,
   NotificationFunctionResult
 } from '../types.js'
@@ -9,7 +16,45 @@ export const sendWorkOrderCreateMsTeamsNotification: NotificationFunction =
     notificationConfiguration: NotificationConfiguration,
     workOrderId: number | string
   ): Promise<NotificationFunctionResult | undefined> => {
-    throw new Error('Not implemented yet')
+    const workOrderToSend = await getWorkOrderToSend(
+      workOrderId,
+      notificationConfiguration
+    )
+
+    if (!workOrderToSend?.success) {
+      return workOrderToSend
+    }
+
+    const workOrder = workOrderToSend.workOrder
+
+    const msTeamsSpecificConfig = JSON.parse(
+      notificationConfiguration.notificationTypeFormJson
+    ) as MsTeamsNotificationConfig
+
+    await sendMessageToTeamsWebhook(
+      msTeamsSpecificConfig.webhookUrl,
+      [
+        {
+          type: 'TextBlock',
+
+          text: `New ${workOrder.workOrderType}: ${workOrder.workOrderNumber}`,
+          weight: 'bolder'
+        },
+        recordToFactSet({
+          Priority: workOrder.workOrderPriorityDataListItem,
+          Status: workOrder.workOrderStatusDataListItem,
+          Details: workOrder.workOrderDetails
+        })
+      ],
+      {
+        type: 'Action.OpenUrl',
+
+        title: `View ${getConfigProperty('workOrders.sectionNameSingular')}`,
+        url: getWorkOrderUrl(workOrder.workOrderId)
+      }
+    )
+
+    return { success: true }
   }
 
 export const sendWorkOrderUpdateMsTeamsNotification: NotificationFunction =
@@ -17,5 +62,44 @@ export const sendWorkOrderUpdateMsTeamsNotification: NotificationFunction =
     notificationConfiguration: NotificationConfiguration,
     workOrderId: number | string
   ): Promise<NotificationFunctionResult | undefined> => {
-    throw new Error('Not implemented yet')
+    
+    const workOrderToSend = await getWorkOrderToSend(
+      workOrderId,
+      notificationConfiguration
+    )
+
+    if (!workOrderToSend?.success) {
+      return workOrderToSend
+    }
+
+    const workOrder = workOrderToSend.workOrder
+
+    const msTeamsSpecificConfig = JSON.parse(
+      notificationConfiguration.notificationTypeFormJson
+    ) as MsTeamsNotificationConfig
+
+    await sendMessageToTeamsWebhook(
+      msTeamsSpecificConfig.webhookUrl,
+      [
+        {
+          type: 'TextBlock',
+
+          text: `Updated ${workOrder.workOrderType}: ${workOrder.workOrderNumber}`,
+          weight: 'bolder'
+        },
+        recordToFactSet({
+          Priority: workOrder.workOrderPriorityDataListItem,
+          Status: workOrder.workOrderStatusDataListItem,
+          Details: workOrder.workOrderDetails
+        })
+      ],
+      {
+        type: 'Action.OpenUrl',
+
+        title: `View ${getConfigProperty('workOrders.sectionNameSingular')}`,
+        url: getWorkOrderUrl(workOrder.workOrderId)
+      }
+    )
+
+    return { success: true }
   }
